d8ba054f03e9648762174ebcd7be8656187f605f
Sudarshan Zimal <98067580+SudarshanZimal@users.noreply.github.com>
2024-03-17 10:53:41 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (20.46%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,19 +1,14 @@
 class Solution {
-    public static int returNlength(String s) {
-		int ind = s.lastIndexOf(" ");
-		int result = 0;
-		if(ind!=-1 && ind!=s.length()-1) {
-			String ss = s.substring(ind,s.length()-1);
-			result = ss.length();
-		}else if(ind!=-1 && ind==s.length()-1) {
-			String ss = s.substring(0,ind);
-			result = returNlength(ss);
-		}else if(ind==-1){
-			result = s.length();
-		}
-		return result;
-	}
     public int lengthOfLastWord(String s) {
-    	return returNlength(s);		 
+        String trim = s.trim();
+        char[] ch = trim.toCharArray();
+        int cnt =0;
+        for(int i = ch.length-1;i>=0;i--) {
+            cnt++;
+            if(ch[i]==' ') {
+                return cnt-1;
+            }
+        }
+        return cnt;
     }
 }
