38ec156fc47cfc7552c136153ecc69668450f9bf
Sudarshan Zimal <98067580+SudarshanZimal@users.noreply.github.com>
2024-03-17 15:29:11 +0530

    Time: 2 ms (77.39%), Space: 61.7 MB (55.21%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,26 +1,16 @@
-import java.util.Arrays;
-
-public class Solution {
+class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-        int[] maxPrices = new int[n];
-        maxPrices[n - 1] = prices[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            maxPrices[i] = Math.max(maxPrices[i + 1], prices[i]);
+        int lowest = Integer.MAX_VALUE;
+        int highestRange = 0;
+        
+        for (int price : prices) {
+            if (price < lowest) {
+                lowest = price;
+            }
+            else if (price - lowest > highestRange) {
+                highestRange = price - lowest;
+            }
         }
-        int maxProfit = 0;
-        for (int i = 0; i < n; i++) {
-            maxProfit = Math.max(maxProfit, maxPrices[i] - prices[i]);
-        }
-        return maxProfit;
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int[] prices1 = {7, 1, 5, 3, 6, 4};
-        System.out.println(solution.maxProfit(prices1)); // Output: 5
-
-        int[] prices2 = {7, 6, 4, 3, 1};
-        System.out.println(solution.maxProfit(prices2)); // Output: 0
+        return highestRange;
     }
 }
