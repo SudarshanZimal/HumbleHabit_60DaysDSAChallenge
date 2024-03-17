22c760eed4a8ffdc751fd024d74e788f10c79b9f
Sudarshan Zimal <98067580+SudarshanZimal@users.noreply.github.com>
2024-03-17 15:26:39 +0530

    Time: 2 ms (77.39%), Space: 62.1 MB (9.66%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,17 +1,23 @@
-class Solution {
+import java.util.Arrays;
+
+public class Solution {
     public int maxProfit(int[] prices) {
-        int i=0,j=1;
-        int max=0;
-        while(j<prices.length){
-            if(prices[j]<prices[i]){
-                i=j;
-                j++;
-            }
-            else{
-            max=Math.max(max,prices[j]-prices[i]);
-            j++;
-            }
+        int n = prices.length;
+        int buyAtPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i = 0; i < n; i++) {
+            buyAtPrice = Math.min(buyAtPrice, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i] - buyAtPrice);
         }
-        return max;
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] prices1 = {7, 1, 5, 3, 6, 4};
+        System.out.println(solution.maxProfit(prices1)); // Output: 5
+
+        int[] prices2 = {7, 6, 4, 3, 1};
+        System.out.println(solution.maxProfit(prices2)); // Output: 0
     }
 }
