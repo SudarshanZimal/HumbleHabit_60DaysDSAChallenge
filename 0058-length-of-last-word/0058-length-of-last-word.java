010da3b41c6f81e33b9f9b8f57a2fc797333650e
Sudarshan Zimal <98067580+SudarshanZimal@users.noreply.github.com>
2024-03-17 10:43:27 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (78.48%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,13 +1,10 @@
-import java.util.List;
-import java.util.Arrays;
-
-public class Solution {
+class Solution {
     public int lengthOfLastWord(String s) {
-        List<String> words = Arrays.asList(s.split(" "));
-        int i = words.size() - 1;
-        while (i >= 0 && words.get(i).isEmpty()) {
-            i--;
-        }
-        return words.get(i).length();
+        s =  s.trim();
+        int str = s.length()-1;
+        int i;
+        for(i=str;i>=0;i--) 
+            if(s.charAt(i) == ' ') break;  
+        return (str-i);
     }
-}
+ }
